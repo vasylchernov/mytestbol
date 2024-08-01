791f2377cb6a6a539167aa230805b12a22a241aa
VasylChernov <vasyl@dubbelglas.nu>
2024-08-01 08:47:35 +0200

    created test branch tailwind_2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>tailwind</title>
+        <title>tailwind - 2</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,7 +17,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+{{--            @include('layouts.navigation')--}}
 
             <!-- Page Heading -->
             @isset($header)
--- resources/views/test/tailwindcss.blade.php
+++ resources/views/test/tailwindcss.blade.php
@@ -26,4 +26,29 @@
         </div>
     </div>
 
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-xl flex items-center space-x-4">
+        <div class="shrink-0">
+            <img class="size-12" src="{{ asset('storage/images/user-icon.png') }}" alt="ChitChat Logo">
+        </div>
+        <div>
+            <div class="text-xl font-medium text-black">ChitChat</div>
+            <p class="text-slate-500">You have a new message!</p>
+        </div>
+    </div>
+
+    <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="{{ asset('storage/images/w_face.png') }}" alt="Woman's Face" />
+        <div class="text-center space-y-2 sm:text-left">
+            <div class="space-y-0.5">
+                <p class="text-lg text-black font-semibold">
+                    Erin Lindford
+                </p>
+                <p class="text-slate-500 font-medium">
+                    Product Engineer
+                </p>
+            </div>
+            <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+        </div>
+    </div>
+
 </x-app-layout>
