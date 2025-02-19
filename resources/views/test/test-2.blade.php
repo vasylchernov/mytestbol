39359c79cd4e61796919c85fd098d4af00664ad8
it-dubbelglas <it@dubbelglas.nu>
2025-02-19 09:08:23 +0100

    test commit

--- resources/views/test/test-2.blade.php
+++ resources/views/test/test-2.blade.php
@@ -1,25 +1,36 @@
 <x-app-layout>
 
-    <div>
-        <livewire:example-component />
+    {{--    <div>--}}
+    {{--        <livewire:example-component />--}}
+    {{--    </div>--}}
+    {{--    <div>--}}
+    {{--        hello world!--}}
+    {{--    </div>--}}
+
+    {{--    <div>--}}
+    {{--        @foreach($data as $it)--}}
+    {{--            <p>{{ $it }}</p>--}}
+    {{--        @endforeach--}}
+    {{--    </div>--}}
+
+    <div class="p-6 max-w-xl mx-auto my_dark dark items-center"><br><br>
+        <div class="max-w-xl bg-amber-50 justify-center text-center">
+            @foreach($data as $it)
+                <h1 class="font-bold">{{ $it }}</h1>
+            @endforeach
+        </div>
     </div>
 
-    <div>
-        @foreach($data as $it)
-            <p>{{ $it }}</p>
-        @endforeach
-    </div>
+    {{--    <div class="p-6 max-w-xl mx-auto my_dark dark"><br><br>--}}
+    {{--        <p>Tripleglas bestellen is heel eenvoudig.&nbsp;</p><ul><li>Triple Glas HR+++ heeft argon gasvulling en een coating op de binnen en buitenruit.&nbsp;</li><li>Triple Glas HR+++ Super heeft een verbeterde coating en is daarmee nog beter isolerend.&nbsp;</li><li>Wilt u maximale kwaliteit en extra lange garantie ? dan kiest u voor Triple Glas HR+++ 15 jaar Garantie.</li></ul><p>Triple Glas is er ook met een extra functie:&nbsp; Zonwerend Tripleglas of Geluidswerend Tripleglas.</p>--}}
+    {{--    </div>--}}
 
-    <div class="p-6 max-w-xl mx-auto my_dark dark"><br><br>
-        <p>Tripleglas bestellen is heel eenvoudig.&nbsp;</p><ul><li>Triple Glas HR+++ heeft argon gasvulling en een coating op de binnen en buitenruit.&nbsp;</li><li>Triple Glas HR+++ Super heeft een verbeterde coating en is daarmee nog beter isolerend.&nbsp;</li><li>Wilt u maximale kwaliteit en extra lange garantie ? dan kiest u voor Triple Glas HR+++ 15 jaar Garantie.</li></ul><p>Triple Glas is er ook met een extra functie:&nbsp; Zonwerend Tripleglas of Geluidswerend Tripleglas.</p>
-    </div>
-
-    <div>
-        @forelse($data as $it)
-            {{ $it }}
-        @empty
-            no data found
-        @endforelse
-    </div>
+    {{--    <div>--}}
+    {{--        @forelse($data as $it)--}}
+    {{--            {{ $it }}--}}
+    {{--        @empty--}}
+    {{--            no data found--}}
+    {{--        @endforelse--}}
+    {{--    </div>--}}
 
 </x-app-layout>
